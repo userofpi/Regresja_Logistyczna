58f4d1ea2aa3889b6c8933c660bf508e0fa9160f
userofpi <maciej.klimdziuk@student.wat.edu.pl>
2025-01-22 19:37:16 +0100

    Organized

---
+++ Analiza_Platkow_Sniadaniowych/platki4.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+data2 = pd.read_csv('Analiza_Platkow_Sniadaniowych/dane_platki_srednie.csv', sep=';', header=0)
+data2
--- platki4.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-data2 = pd.read_csv('dane_platki_srednie.csv', sep=';', header=0)
-data2
